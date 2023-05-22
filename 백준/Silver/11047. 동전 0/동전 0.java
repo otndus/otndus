a2f0566fbc4b2ae5c1073ac78c4a4374e38ae8ac
SUYEON OH <97380428+otndus@users.noreply.github.com>
2023-05-22 22:30:07 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int coin = 0;
+        while(K != 0){
+            for(int i=N-1; i>=0; i--){
+                if (K / arr[i] > 0) {
+                    coin += K / arr[i];
+                    K %= arr[i];
+                }
+            }
+        }
+        System.out.println(coin);
+
+    }
+}
